7ed8d9b91869512cba2421daf4b7b6f37c45bad0
Andrew Tesla <korvin7.64@gmail.com>
2017-10-25 15:15:23 +0500

    basr login screen layout

---
+++ app/src/main/java/ru/xfit/screens/auth/AuthController.java
@@ -0,0 +1,17 @@
+package ru.xfit.screens.auth;
+
+import ru.xfit.R;
+import ru.xfit.databinding.LayoutAuthBinding;
+import ru.xfit.screens.XFitController;
+
+/**
+ * Created by TESLA on 25.10.2017.
+ */
+
+public class AuthController extends XFitController<LayoutAuthBinding> {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.layout_auth;
+    }
+}
