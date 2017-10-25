1c81be133301af696585f413ee3f9ac48d02a31f
Andrew Tesla <korvin7.64@gmail.com>
2017-10-26 00:36:34 +0500

    added register controller, base layout

--- app/src/main/java/ru/xfit/screens/auth/RegisterController.java
+++ app/src/main/java/ru/xfit/screens/auth/RegisterController.java
@@ -1,5 +1,8 @@
 package ru.xfit.screens.auth;
 
+import android.databinding.ObservableField;
+import android.view.View;
+
 import ru.xfit.R;
 import ru.xfit.databinding.LayoutRegisterBinding;
 import ru.xfit.screens.XFitController;
@@ -9,8 +12,15 @@ import ru.xfit.screens.XFitController;
  */
 
 public class RegisterController extends XFitController<LayoutRegisterBinding> {
+    public ObservableField<String> phone = new ObservableField<>("");
+    public ObservableField<String> password = new ObservableField<>("");
+
     @Override
     public int getLayoutId() {
         return R.layout.layout_register;
+    }
+
+    public void register(View view) {
+
     }
 }
