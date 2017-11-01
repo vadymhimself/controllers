11fbbbcf73a8562a869adc84d04a187c3dfb1465
Andrew Tesla <korvin7.64@gmail.com>
2017-11-02 02:16:18 +0500

    added changing month by arrows

---
+++ app/src/main/java/ru/xfit/misc/views/OnMonthTitleClickListener.java
@@ -0,0 +1,13 @@
+package ru.xfit.misc.views;
+
+import android.support.annotation.NonNull;
+
+import java.util.Date;
+
+/**
+ * Created by TESLA on 02.11.2017.
+ */
+
+public interface OnMonthTitleClickListener {
+    void onMonthTitleClick(@NonNull Date monthDate);
+}
