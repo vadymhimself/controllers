a7363b2411f63f50c605faa40502cde488e096c1
alekseismovzhenko <alekseismovzhenko@gmail.com>
2017-12-07 23:14:03 +0200

    next try fix travis

--- .travis-android-license-bugfix.sh
+++
@@ -1,4 +0,0 @@
-echo "$ANDROID_HOME"
-mkdir "$ANDROID_HOME/licenses" || true
-echo -e "\nd56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license"
-echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
