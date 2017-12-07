c690d6fc619b886c1c01c3248c1fcb1999dc1b57
alekseismovzhenko <alekseismovzhenko@gmail.com>
2017-12-07 17:47:20 +0200

    #XF-311, #XF-349 added password validation with entropy

--- app/src/main/java/ru/xfit/misc/utils/validation/PasswordValidator.java
+++ app/src/main/java/ru/xfit/misc/utils/validation/PasswordValidator.java
@@ -19,7 +19,40 @@ public class PasswordValidator extends StringValidator {
             return result;
         if (!PASSWORD.matcher(args[0]).matches())
             return App.getContext().getString(R.string.incorrect_password);
+        if (calculateEnthropy(args[0]) < 42)
+            return App.getContext().getString(R.string.easy_password);
         return null;
 
+    }
+
+
+    private double calculateEnthropy(String string) {
+        int enthropy = 0;
+        char[] symbols = string.toCharArray();
+        for (char symbol : symbols) {
+            if (Character.isLowerCase(symbol)) {
+                enthropy += 26;
+                break;
+            }
+        }
+        for (char symbol : symbols) {
+            if (Character.isUpperCase(symbol)) {
+                enthropy += 26;
+                break;
+            }
+        }
+        for (char symbol : symbols) {
+            if (Character.isDigit(symbol)) {
+                enthropy += 10;
+                break;
+            }
+        }
+        for (char symbol : symbols) {
+            if ("\\|¬¦`!\"£$%^&*()_+-=[]{};:'@#~<>,./? ".indexOf(symbol) >= 0) {
+                enthropy += 36;
+                break;
+            }
+        }
+        return Math.log(enthropy) / Math.log(2) * symbols.length;
     }
 }
