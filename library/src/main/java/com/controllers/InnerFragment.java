3fa84369a89488e83c05e19c7530291a11ed5ce3
alekseismovzhenko <alekseismovzhenko@gmail.com>
2018-01-03 23:20:16 +0200

    fixed versions, fixed controllers retained state

--- library/src/main/java/com/controllers/InnerFragment.java
+++ library/src/main/java/com/controllers/InnerFragment.java
@@ -1,5 +1,6 @@
 package com.controllers;
 
+import android.content.res.Resources;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.os.Bundle;
@@ -10,8 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
 import com.controllers.AbstractController.ViewLifecycleConsumer;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -145,42 +151,42 @@ public final class InnerFragment<B extends ViewDataBinding> extends Fragment {
         }
     }
 
-//    private static final int DEFAULT_CHILD_ANIMATION_DURATION = 250;
-//
-//    @Override
-//    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
-//        final Fragment parent = getParentFragment();
-//
-//        // Apply the workaround only if this is a child fragment, and the parent
-//        // is being removed.
-//        if (!enter && parent != null && parent.isRemoving()) {
-//            // This is a workaround for the bug where child fragments disappear when
-//            // the parent is removed (as all children are first removed from the parent)
-//            // See https://code.google.com/p/android/issues/detail?id=55228
-//            Animation doNothingAnim = new AlphaAnimation(1, 1);
-//            doNothingAnim.setDuration(getNextAnimationDuration(parent, DEFAULT_CHILD_ANIMATION_DURATION));
-//            return doNothingAnim;
-//        } else {
-//            return super.onCreateAnimation(transit, enter, nextAnim);
-//        }
-//    }
-//
-//    private static long getNextAnimationDuration(Fragment fragment, long defValue) {
-//        try {
-//            // Attempt to get the resource ID of the next animation that
-//            // will be applied to the given fragment.
-//            Field nextAnimField = Fragment.class.getDeclaredField("mNextAnim");
-//            nextAnimField.setAccessible(true);
-//            int nextAnimResource = nextAnimField.getInt(fragment);
-//            Animation nextAnim = AnimationUtils.loadAnimation(fragment.getActivity(), nextAnimResource);
-//
-//            // ...and if it can be loaded, return that animation's duration
-//            return (nextAnim == null) ? defValue : nextAnim.getDuration();
-//        } catch (NoSuchFieldException|IllegalAccessException|Resources.NotFoundException ex) {
-//            Log.w(InnerFragment.class.getSimpleName(), "Unable to load next animation from parent.", ex);
-//            return defValue;
-//        }
-//    }
+    private static final int DEFAULT_CHILD_ANIMATION_DURATION = 250;
+
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+        final Fragment parent = getParentFragment();
+
+        // Apply the workaround only if this is a child fragment, and the parent
+        // is being removed.
+        if (!enter && parent != null && parent.isRemoving()) {
+            // This is a workaround for the bug where child fragments disappear when
+            // the parent is removed (as all children are first removed from the parent)
+            // See https://code.google.com/p/android/issues/detail?id=55228
+            Animation doNothingAnim = new AlphaAnimation(1, 1);
+            doNothingAnim.setDuration(getNextAnimationDuration(parent, DEFAULT_CHILD_ANIMATION_DURATION));
+            return doNothingAnim;
+        } else {
+            return super.onCreateAnimation(transit, enter, nextAnim);
+        }
+    }
+
+    private static long getNextAnimationDuration(Fragment fragment, long defValue) {
+        try {
+            // Attempt to get the resource ID of the next animation that
+            // will be applied to the given fragment.
+            Field nextAnimField = Fragment.class.getDeclaredField("mNextAnim");
+            nextAnimField.setAccessible(true);
+            int nextAnimResource = nextAnimField.getInt(fragment);
+            Animation nextAnim = AnimationUtils.loadAnimation(fragment.getActivity(), nextAnimResource);
+
+            // ...and if it can be loaded, return that animation's duration
+            return (nextAnim == null) ? defValue : nextAnim.getDuration();
+        } catch (NoSuchFieldException|IllegalAccessException|Resources.NotFoundException ex) {
+            Log.w(InnerFragment.class.getSimpleName(), "Unable to load next animation from parent.", ex);
+            return defValue;
+        }
+    }
 
     void subscribe(ViewLifecycleConsumer consumer) {
         consumers.add(consumer);
