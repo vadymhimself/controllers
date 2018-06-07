ddb5c275362bd2d6c13567e6f9df5f6417d69ae5
bolein <bolein95@gmail.com>
2018-06-06 19:46:27 -0500

    implemented coroutines module

---
+++ coroutines/src/main/java/KotlinController.kt
@@ -0,0 +1,24 @@
+package com.controllers
+
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
+
+import kotlinx.coroutines.experimental.CommonPool as BG_CONTEXT
+import kotlinx.coroutines.experimental.android.UI as UI_CONTEXT
+
+
+
+inline fun <T : Controller<*>> T.async(crossinline block: suspend AsyncHandle.() -> Unit) {
+    launch(UI_CONTEXT) {
+        val asyncHandle = AsyncHandle()
+        block(asyncHandle)
+    }
+}
+
+class AsyncHandle {
+    suspend fun <R> await(block: suspend () -> R) : R {
+        return withContext(BG_CONTEXT) {
+            block()
+        }
+    }
+}
