afbbe4ef944acd0db465cef27489af0917a3eaf2
frozmen <sisetskyi.a@gmail.com>
2018-06-20 12:52:58 +0300

    remove RefreshListener

--- misc/src/main/java/com/controllers/misc/views/RefreshListener.java
+++
@@ -1,8 +0,0 @@
-package com.controllers.misc.views;
-
-
-import android.support.v4.widget.SwipeRefreshLayout;
-
-public interface RefreshListener {
-    void onRefresh(SwipeRefreshLayout refreshLayout);
-}
