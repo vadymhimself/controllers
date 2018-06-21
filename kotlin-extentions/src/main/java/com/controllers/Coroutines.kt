ebb2f2b8209f9ce59f2cad4ad4d0771e76118049
bolein <bolein95@gmail.com>
2018-06-21 18:44:06 -0500

    fixed coroutine cancellation

--- kotlin-extentions/src/main/java/com/controllers/Coroutines.kt
+++ kotlin-extentions/src/main/java/com/controllers/Coroutines.kt
@@ -2,80 +2,77 @@ package com.controllers
 
 import android.util.Log
 import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.cancel
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.withContext
 import java.net.UnknownHostException
+import kotlin.coroutines.experimental.CoroutineContext
 
 import kotlinx.coroutines.experimental.CommonPool as BG_CONTEXT
 import kotlinx.coroutines.experimental.android.UI as UI_CONTEXT
 
 /**
  * TODO: postpone coroutine launch until controller is attached
+ * TODO: make a child of activity's job
  */
 fun <T : Controller<*>> T.async(block: suspend AsyncHandle.() -> Unit): Job {
 
-    val job = Job()
-    // subscribe Job object to controller's lifecycle
-    this.observeWith(JobObserver(job))
+  // take a snapshot of the stack trace in order to make the exception inside
+  // the coroutine more readable
+  val snapshot = Exception()
 
-    // take a snapshot of the stack trace in order to make the exception inside
-    // the coroutine more readable
-    val snapshot = Exception()
-
-    // launch a new UI coroutine in the context of new job
-    launch(UI_CONTEXT, parent = job) {
-        val asyncHandle = AsyncHandle()
-        try {
-            block(asyncHandle)
-        } catch (e: Exception) {
-            val ex = Exception("Runtime exception in $coroutineContext", e)
-            ex.stackTrace = snapshot.stackTrace
-            if (e is UnknownHostException || e.cause is UnknownHostException) { // TODO check cause recursively
-                // Log does not print stack of the UnknownHostException
-                ex.printStackTrace()
-            } else {
-                Log.e(coroutineContext.toString(), "Runtime exception", ex)
-            }
-            throw ex
-        }
+  // launch a new UI coroutine in the context of new job
+  return launch(UI_CONTEXT) {
+    val jobObserver = JobObserver(coroutineContext)
+    this@async.addObserver(jobObserver)
+    try {
+      block(AsyncHandle())
+    } catch (e: Exception) {
+      val ex = Exception("Runtime exception in $coroutineContext", e)
+      ex.stackTrace = snapshot.stackTrace
+      // TODO check cause recursively
+      if (e is UnknownHostException || e.cause is UnknownHostException) {
+        // Log does not print stack of the UnknownHostException
+        ex.printStackTrace()
+      } else {
+        Log.e(coroutineContext.toString(), "Runtime exception", ex)
+      }
+      throw ex
+    } finally {
+      this@async.removeObserver(jobObserver)
     }
-
-    return job
-}
-
-@PublishedApi
-internal fun <T : ObservableController<*>> T.observeWith(obs: ObservableController.Observer) {
-    this.addObserver(obs)
+  }
 }
 
 class AsyncHandle {
-    suspend inline fun <R> await(crossinline block: suspend () -> R) : R {
-        return withContext(BG_CONTEXT) {
-            block()
-        }
+  suspend inline fun <R> await(crossinline block: suspend () -> R): R {
+    return withContext(BG_CONTEXT) {
+      block()
     }
+  }
 }
 
-class JobObserver(private val job : Job) : ObservableController.Observer {
-    override fun onAttachedToStack(observable: ObservableController<*>) {
+class JobObserver(private val context: CoroutineContext) : ObservableController.Observer {
+  override fun onAttachedToStack(observable: ObservableController<*>) {
 
-    }
+  }
 
-    override fun onDetachedFromStack(observable: ObservableController<*>) {
-        job.cancel()
-        observable.removeObserver(this)
-    }
+  override fun onDetachedFromStack(observable: ObservableController<*>) {
+    // TODO may be null if context is deserialized
+    context.cancel()
+    observable.removeObserver(this)
+  }
 
-    override fun onAttachedToScreen(observable: ObservableController<*>) {
+  override fun onAttachedToScreen(observable: ObservableController<*>) {
 
-    }
+  }
 
-    override fun onDetachedFromScreen(observable: ObservableController<*>) {
+  override fun onDetachedFromScreen(observable: ObservableController<*>) {
 
-    }
+  }
 
-    override fun onRestored(observable: ObservableController<*>) {
+  override fun onRestored(observable: ObservableController<*>) {
 
-    }
+  }
 
 }
--- kotlin-extentions/src/main/java/com/controllers/DataBinding.kt
+++ kotlin-extentions/src/main/java/com/controllers/DataBinding.kt
@@ -1,23 +1,31 @@
 package com.controllers
 
 import android.databinding.BaseObservable
+import java.io.Serializable
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class BindableProperty<in R : BaseObservable, T>(
-        private val propBrId: Int,
-        initializer: () -> T? = { null }
-) : ReadWriteProperty<R, T?> {
+  private val propBrId: Int,
+  initializer: () -> T? = { null }
+) : ReadWriteProperty<R, T?>, Serializable {
 
-    var field: T? = initializer()
+  var field: T? = initializer()
 
-    override fun setValue(thisRef: R, property: KProperty<*>, value: T?) {
-        field = value
-        thisRef.notifyPropertyChanged(propBrId)
-    }
+  override fun setValue(
+    thisRef: R,
+    property: KProperty<*>,
+    value: T?
+  ) {
+    field = value
+    thisRef.notifyPropertyChanged(propBrId)
+  }
 
-    override fun getValue(thisRef: R, property: KProperty<*>): T? {
-        return field
-    }
+  override fun getValue(
+    thisRef: R,
+    property: KProperty<*>
+  ): T? {
+    return field
+  }
 
 }
