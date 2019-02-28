65a8fb11161dac1c1480384aec772d9acc57db18
vladyslavsosnovskyi <vladislav.sosnovskyi@gmail.com>
2019-02-28 16:34:18 +0200

    add new map search

--- misc/src/main/java/com/controllers/misc/adapters/binding/TextViewAdapters.java
+++ misc/src/main/java/com/controllers/misc/adapters/binding/TextViewAdapters.java
@@ -4,7 +4,6 @@ import android.databinding.BindingAdapter;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
-import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.text.Html;
@@ -19,7 +18,7 @@ public abstract class TextViewAdapters {
 
     @BindingAdapter(value = {"android:drawableRight", "android:drawableTint"}, requireAll = true)
     public static void _bindDrawableTint(TextView tv, @DrawableRes int drawableRes, @ColorRes Integer colorRes) {
-        Drawable d = VectorDrawableCompat.create(tv.getResources(), drawableRes, null);
+        Drawable d = ContextCompat.getDrawable(tv.getContext(), drawableRes);
         if(d != null){
             d = d.mutate();
             DrawableCompat.setTint(d, ContextCompat.getColor(tv.getContext(), colorRes));
@@ -29,7 +28,7 @@ public abstract class TextViewAdapters {
 
     @BindingAdapter(value = {"android:drawableLeft", "android:drawableTint"}, requireAll = true)
     public static void _bindDrawableLeftTint(TextView tv, @DrawableRes int drawableRes, @ColorRes Integer colorRes) {
-        Drawable d = VectorDrawableCompat.create(tv.getResources(), drawableRes, null);
+        Drawable d = ContextCompat.getDrawable(tv.getContext(), drawableRes);
         if(d != null){
           d = d.mutate();
           DrawableCompat.setTint(DrawableCompat.unwrap(d), ContextCompat.getColor(tv.getContext(), colorRes));
